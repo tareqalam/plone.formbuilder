d2e5cf9a1e683eb4b5cd3324caaa4efc95e59c2f
tareqalam <tareq.mist@gmail.com>
2017-10-22 18:42:36 +0600

    added views to list contents

---
+++ src/plone/formbuilder/browser/schemaformdata_list.py
@@ -0,0 +1,11 @@
+
+# from plone import api
+from Products.Five.browser import BrowserView
+
+# import simplejson as json
+# from plone.protect.auto import safeWrite
+
+
+class SchemaFormDataList(BrowserView):
+    """
+    """
